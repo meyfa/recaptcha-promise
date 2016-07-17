228d29d59615440a9fbf92ad8743032009493e63
JangoBrick <mail@jangobrick.de>
2016-07-17 15:06:20 +0200

    Add initial code & package.json
    * The package.json is configured with repo metadata, a version of 0.1.0,
    and the required dependencies.
    * The index.js file is added, exporting a function for verifying a
    response. To set up the secret key, call .init() on the export.

---
+++ index.js
@@ -0,0 +1,77 @@
+/*jshint node: true */
+
+var Promise = require("bluebird");
+var request = require("request");
+
+
+
+/**
+ * URL of the verification endpoint
+ */
+var url = "https://www.google.com/recaptcha/api/siteverify";
+
+/**
+ * The options object, set via .init()
+ */
+var options = {};
+
+
+
+
+
+/**
+ * Takes a user response and optionally the remote address. Then performs a
+ * request to Google for verifying the response using the secret set via .init()
+ * and returns the response's validity as a Promise.
+ */
+module.exports = function verify(response, remoteAddress) {
+
+    return new Promise(function (fulfill, reject) {
+
+        var params = {
+            form: {
+                secret: options.secret_key,
+                response: response
+            }
+        };
+
+        if (remoteAddress) {
+            params.form.remoteip = remoteAddress;
+        }
+
+        request.post(url, params, function (err, res, body) {
+
+            if (err) {
+                // some request error occurred
+                return reject(err);
+            } else if (res.statusCode != 200) {
+                // error reported by ReCAPTCHA endpoint
+                return reject(new Error("recaptcha status " + res.statusCode));
+            }
+
+            fulfill(JSON.parse(body).success);
+
+        });
+
+    });
+
+};
+
+
+
+/**
+ * Sets the config object to use for future requests. The object may contain:
+ *   - secret_key: the site's secret key to use for validation
+ * Returns this module, so that direct chaining with a verify call is possible.
+ */
+module.exports.init = function (config) {
+
+    if (typeof config !== "object") {
+        throw new Error("config must be an object");
+    }
+
+    options = config;
+
+    return module.exports;
+
+};
