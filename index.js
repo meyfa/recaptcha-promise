12250439206f941577070abe431cbf4f054a61f7
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-10-08 02:02:57 +0200

    Use JS Standard style, add package lock

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
-/*jshint node: true */
+'use strict'
 
-var Promise = require("bluebird");
-var request = require("request");
+const Promise = require('bluebird')
+const request = require('request')
 
 /**
  * URL of the verification endpoint
  */
-var url = "https://www.google.com/recaptcha/api/siteverify";
+const url = 'https://www.google.com/recaptcha/api/siteverify'
 
 /**
  * The options object, set via .init()
  */
-var options = {};
+let options = {}
 
 /**
  * Takes a user response and optionally the remote address. Then performs a
  * request to Google for verifying the response using the secret set via .init()
  * and returns the response's validity as a Promise.
  */
-module.exports = function verify(response, remoteAddress) {
+module.exports = function verify (response, remoteAddress) {
+  return new Promise(function (resolve, reject) {
+    const params = {
+      form: {
+        secret: options.secret_key,
+        response: response
+      }
+    }
+    if (remoteAddress) {
+      params.form.remoteip = remoteAddress
+    }
 
-    return new Promise(function (fulfill, reject) {
-
-        var params = {
-            form: {
-                secret: options.secret_key,
-                response: response
-            }
-        };
-        if (remoteAddress) {
-            params.form.remoteip = remoteAddress;
-        }
-
-        request.post(url, params, function (err, res, body) {
-            if (err) {
-                // some request error occurred
-                return reject(err);
-            } else if (res.statusCode != 200) {
-                // error reported by ReCAPTCHA endpoint
-                return reject(new Error("recaptcha status " + res.statusCode));
-            }
-            fulfill(JSON.parse(body).success);
-        });
-
-    });
-
-};
+    request.post(url, params, function (err, res, body) {
+      if (err) {
+        // some request error occurred
+        return reject(err)
+      } else if (res.statusCode !== 200) {
+        // error reported by ReCAPTCHA endpoint
+        return reject(new Error('recaptcha status ' + res.statusCode))
+      }
+      resolve(JSON.parse(body).success)
+    })
+  })
+}
 
 /**
  * Sets the config object to use for future requests. The object may contain:
@@ -53,9 +49,9 @@ module.exports = function verify(response, remoteAddress) {
  * Returns this module, so that direct chaining with a verify call is possible.
  */
 module.exports.init = function (config) {
-    if (typeof config !== "object") {
-        throw new Error("config must be an object");
-    }
-    options = config;
-    return module.exports;
-};
+  if (typeof config !== 'object') {
+    throw new Error('config must be an object')
+  }
+  options = config
+  return module.exports
+}
