dee22509045899d0da19987e8da63abc40c17042
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-10-08 16:13:07 +0200

    Refactor to allow creating multiple instances

--- README.md
+++ README.md
@@ -3,32 +3,68 @@
 This Node module aims to provide simple, asynchronous, promise-based ReCAPTCHA
 response verification.
 
-This software is in no way affiliated with or endorsed by Google.
-Google and the Google Logo are registered trademarks of Google Inc.
+_This software is in no way affiliated with or endorsed by Google._
+
+
+## Installation
+
+Add to your project's dependencies:
+
+```sh
+npm install recaptcha-promise
+```
 
 
 ## Usage
 
-```
-npm install recaptcha-promise
-```
+**UPDATE NOTICE**: If you are currently using version 0.x of this package, fear
+not! Everything still works the same in this version. Nonetheless, if you do
+have the time, consider upgrading to the newer usage patterns.
+
+### Creating an instance
+
+This is the recommended way. First, an instance is created with your secret key.
+It can then be used to verify as many challenges as needed.
 
 ```js
-const recaptcha = require('recaptcha-promise')
-
-recaptcha.init({
-  secret_key: 'YOUR_SECRET_KEY'
+const recaptcha = require('recaptcha-promise').create({
+  secret: 'YOUR_SECRET_KEY'
 })
 
-// with Promises (remoteAddress is optional)
-recaptcha(userResponse, remoteAddress).then(function (success) {
+// In an HTTP handler:
+
+// ... with Promises:
+recaptcha.verify(userResponse).then(function (success) {
   console.log(success ? 'Response valid' : 'Response invalid')
 })
 
 // ... or in an async context:
-async function handleRequest (userResponse, remoteAddress) {
-  const success = await recaptcha(userResponse, remoteAddress)
+async function handleRequest (userResponse) {
+  const success = await recaptcha.verify(userResponse)
   console.log(success ? 'Response valid' : 'Response invalid')
 }
 handleRequest(/* ... */)
 ```
+
+### The global instance
+
+This exists mostly for legacy reasons. If you do not want to dependency-inject
+the `RecaptchaVerify` object, you can configure this package with your secret
+key globally:
+
+```js
+// note the missing .create(...) call
+const recaptcha = require('recaptcha-promise')
+
+recaptcha.init({
+  secret: 'YOUR_SECRET_KEY'
+})
+```
+
+You can then use `recaptcha` exactly the same as above. You can also require it
+elsewhere and it will still have the secret key ready.
+
+### Passing a remote address
+
+If you have the client's remote address available, you can pass it as a second
+parameter to the `verify` function.
--- index.js
+++ index.js
@@ -3,44 +3,62 @@
 const axios = require('axios')
 const querystring = require('querystring')
 
+// CONSTANTS
+
 /**
- * URL of the verification endpoint
+ * URL of the verification endpoint.
+ *
+ * @type {string}
  */
 const VERIFY_ENDPOINT = 'https://www.google.com/recaptcha/api/siteverify'
 
 /**
- * The options object, set via .init()
+ * Create a new verifier instance. The returned object exposes a `verify` method
+ * and an `init` method for (re-)configuration. Moreover, the returned object
+ * can be called as a function directly, which is synonymous to calling
+ * `verify`.
+ *
+ * @param {?object} config The initial config object.
+ * @returns {Function} The created verifier instance.
  */
-let options = {}
+function createInstance (config) {
+  let secret = null
 
-/**
- * Takes a user response and optionally the remote address. Then performs a
- * request to Google for verifying the response using the secret set via .init()
- * and returns the response's validity as a Promise.
- */
-async function verify (response, remoteAddress) {
-  const postBody = querystring.stringify({
-    secret: options.secret_key,
-    response: response,
-    remoteip: remoteAddress || undefined
-  })
-  const { data } = await axios.post(VERIFY_ENDPOINT, postBody)
-  return data.success
-}
-
-module.exports = verify
-
-/**
- * Sets the config object to use for future requests. The object may contain:
- *   - secret_key: the site's secret key to use for validation
- * Returns this module, so that direct chaining with a verify call is possible.
- */
-function init (config) {
-  if (typeof config !== 'object') {
-    throw new Error('config must be an object')
+  // this is returned directly, to allow function invocation
+  const verify = async (response, remoteAddress) => {
+    if (!secret) {
+      throw new Error('secret not configured')
+    }
+    const postBody = querystring.stringify({
+      secret: secret,
+      response: response,
+      remoteip: remoteAddress || undefined
+    })
+    const { data } = await axios.post(VERIFY_ENDPOINT, postBody)
+    return data.success
   }
-  options = config
-  return module.exports
+
+  // this is added as a property to allow newer syntax
+  verify.verify = verify
+
+  // this is added to support (re-)configuration
+  verify.init = (config) => {
+    if (typeof config !== 'object' || !config) {
+      throw new Error('config object not provided')
+    }
+    secret = config.secret || config.secretKey || config.secret_key
+    return verify
+  }
+
+  // load initial config if provided
+  if (config) {
+    verify.init(config)
+  }
+
+  return verify
 }
 
-module.exports.init = init
+const globalInstance = createInstance()
+// the global instance should also have a `create` method for customization
+globalInstance.create = createInstance
+module.exports = globalInstance
