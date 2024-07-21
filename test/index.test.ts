8c38cbf76bf4e8046dd54f1155e9dae0e69d9385
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2024-07-21 10:29:32 +0200

    feat: Use native `fetch()` instead of axios (#179)
    Fetch is available in Node.js 18 and later, and allows us to remove the
    only production dependency of this package with a minimal code
    change. Tests, however, need to be largely rewritten using Node.js's
    undici to mock global fetch.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,21 @@
-import assert from 'node:assert'
-import axios from 'axios'
-import MockAdapter from 'axios-mock-adapter'
-
 import recaptcha from '../index.js'
+import assert from 'node:assert'
+import { getGlobalDispatcher, MockAgent, MockPool, setGlobalDispatcher } from 'undici'
 
 describe('main export', function () {
-  const mock = new MockAdapter(axios, { onNoMatch: 'throwException' })
+  const apiPath = '/recaptcha/api/siteverify'
+  const matchAny = (): boolean => true
 
-  afterEach(function () {
-    mock.reset()
-  })
+  function createMockPool (): MockPool {
+    const mockAgent = new MockAgent()
+    mockAgent.disableNetConnect()
+    setGlobalDispatcher(mockAgent)
 
-  after(function () {
-    mock.restore()
-  })
+    return mockAgent.get('https://www.google.com')
+  }
+
+  const oldGlobalDispatcher = getGlobalDispatcher()
+  afterEach(() => setGlobalDispatcher(oldGlobalDispatcher))
 
   describe('#verify()', function () {
     it('is aliased to the main export', function () {
@@ -22,43 +24,59 @@ describe('main export', function () {
     })
 
     it('rejects if unconfigured, does not perform requests', async function () {
+      const mockPool = createMockPool()
+      let callCount = 0
+      mockPool.intercept({ path: matchAny, method: matchAny }).reply(500, () => {
+        ++callCount
+      })
       // @ts-expect-error - intentionally pass undefined
       recaptcha.init({ secret: undefined }) // reset secret
       await assert.rejects(async () => await recaptcha.verify('foo'))
-      assert.strictEqual(mock.history.post.length, 0)
+      assert.strictEqual(callCount, 0)
     })
 
     it('performs a request with correct body if configured', async function () {
+      const mockPool = createMockPool()
       recaptcha.init({ secret: 'test-secret' })
-      mock.onAny().replyOnce(config => {
-        assert.strictEqual(config.url, 'https://www.google.com/recaptcha/api/siteverify')
-        assert.strictEqual(config.method, 'post')
-        assert.strictEqual(config.data, 'secret=test-secret&response=foo&remoteip=bar')
-        const contentType = config.headers != null ? config.headers['Content-Type'] : undefined
+      let callCount = 0
+      mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+        ++callCount
+        assert.strictEqual(config.path, '/recaptcha/api/siteverify')
+        assert.strictEqual(config.method, 'POST')
+        assert.strictEqual(config.body?.toString(), 'secret=test-secret&response=foo&remoteip=bar')
+        const contentType = config.headers != null ? new Headers(config.headers).get('Content-Type') : undefined
         assert.strictEqual(contentType, 'application/x-www-form-urlencoded')
-        return [200, { success: true }]
+        return { success: true }
       })
       await recaptcha.verify('foo', 'bar')
-      assert.strictEqual(mock.history.post.length, 1)
+      assert.strictEqual(callCount, 1)
     })
 
     it('does not pass remoteip if left out', async function () {
+      const mockPool = createMockPool()
       recaptcha.init({ secret: 'test-secret' })
-      mock.onAny().replyOnce(config => {
-        assert.strictEqual(config.data, 'secret=test-secret&response=foo&remoteip=')
-        return [200, { success: true }]
+      let callCount = 0
+      mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+        ++callCount
+        assert.strictEqual(config.body?.toString(), 'secret=test-secret&response=foo&remoteip=')
+        return { success: true }
       })
       await recaptcha.verify('foo')
-      assert.strictEqual(mock.history.post.length, 1)
+      assert.strictEqual(callCount, 1)
     })
 
     it('resolves with success value', async function () {
+      const mockPool = createMockPool()
       recaptcha.init({ secret: 'test-secret' })
       // case 1
-      mock.onAny().replyOnce(200, { success: true })
+      mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+        return { success: true }
+      })
       assert.strictEqual(await recaptcha.verify('foo'), true)
       // case 2
-      mock.onAny().replyOnce(200, { success: false })
+      mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+        return { success: false }
+      })
       assert.strictEqual(await recaptcha.verify('foo'), false)
     })
   })
@@ -78,27 +96,28 @@ describe('main export', function () {
     })
 
     it('does not distinguish between secret, secretKey, secret_key', async function () {
+      const mockPool = createMockPool()
       // case 1
       recaptcha.init({ secret: 'secret' })
-      mock.onAny().replyOnce(config => {
-        assert.strictEqual(config.data, 'secret=secret&response=foo&remoteip=bar')
-        return [200, { success: true }]
+      mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+        assert.strictEqual(config.body?.toString(), 'secret=secret&response=foo&remoteip=bar')
+        return { success: true }
       })
       await recaptcha.verify('foo', 'bar')
 
       // case 2
       recaptcha.init({ secretKey: 'secretKey' } as any)
-      mock.onAny().replyOnce(config => {
-        assert.strictEqual(config.data, 'secret=secretKey&response=foo&remoteip=bar')
-        return [200, { success: true }]
+      mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+        assert.strictEqual(config.body?.toString(), 'secret=secretKey&response=foo&remoteip=bar')
+        return { success: true }
       })
       await recaptcha.verify('foo', 'bar')
 
       // case 2
       recaptcha.init({ secret_key: 'secret_key' } as any)
-      mock.onAny().replyOnce(config => {
-        assert.strictEqual(config.data, 'secret=secret_key&response=foo&remoteip=bar')
-        return [200, { success: true }]
+      mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+        assert.strictEqual(config.body?.toString(), 'secret=secret_key&response=foo&remoteip=bar')
+        return { success: true }
       })
       await recaptcha.verify('foo', 'bar')
     })
@@ -124,33 +143,43 @@ describe('main export', function () {
       })
 
       it('rejects if unconfigured, does not perform requests', async function () {
+        const mockPool = createMockPool()
+        let callCount = 0
+        mockPool.intercept({ path: matchAny, method: matchAny }).reply(500, () => {
+          ++callCount
+        })
         const obj = recaptcha.create()
         await assert.rejects(async () => await obj.verify('foo'))
-        assert.strictEqual(mock.history.post.length, 0)
+        assert.strictEqual(callCount, 0)
       })
 
       it('is independent between instances', async function () {
+        const mockPool = createMockPool()
+
         // @ts-expect-error - intentionally pass undefined
         recaptcha.init({ secret: undefined }) // reset secret
         const instance1 = recaptcha.create({ secret: 'secret1' })
         const instance2 = recaptcha.create({ secret: 'secret2' })
 
         // case 1
-        mock.onAny().replyOnce(config => {
-          assert.strictEqual(config.data, 'secret=secret1&response=foo1&remoteip=bar1')
-          return [200, { success: true }]
+        let callCount1 = 0
+        mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+          ++callCount1
+          assert.strictEqual(config.body?.toString(), 'secret=secret1&response=foo1&remoteip=bar1')
+          return { success: true }
         })
         await instance1.verify('foo1', 'bar1')
-        assert.strictEqual(mock.history.post.length, 1)
+        assert.strictEqual(callCount1, 1)
 
         // case 2
-        mock.reset()
-        mock.onAny().replyOnce(config => {
-          assert.strictEqual(config.data, 'secret=secret2&response=foo2&remoteip=bar2')
-          return [200, { success: true }]
+        let callCount2 = 0
+        mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+          ++callCount2
+          assert.strictEqual(config.body?.toString(), 'secret=secret2&response=foo2&remoteip=bar2')
+          return { success: true }
         })
         await instance2.verify('foo2', 'bar2')
-        assert.strictEqual(mock.history.post.length, 1)
+        assert.strictEqual(callCount2, 1)
       })
     })
 
@@ -171,13 +200,17 @@ describe('main export', function () {
       })
 
       it('allows re-configuration', async function () {
+        const mockPool = createMockPool()
         const obj = recaptcha.create()
         obj.init({ secret: 'test-secret' })
-        mock.onAny().replyOnce(config => {
-          assert.strictEqual(config.data, 'secret=test-secret&response=foo&remoteip=bar')
-          return [200, { success: true }]
+        let callCount = 0
+        mockPool.intercept({ path: apiPath, method: 'POST' }).reply(200, (config) => {
+          ++callCount
+          assert.strictEqual(config.body?.toString(), 'secret=test-secret&response=foo&remoteip=bar')
+          return { success: true }
         })
         await obj.verify('foo', 'bar')
+        assert.strictEqual(callCount, 1)
       })
     })
   })
